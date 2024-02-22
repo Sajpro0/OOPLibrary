cfd4a13de3d8280c9b227db10c4894d02f5c6379
Korinek Sajpro <pannekdo6@gmail.com>
2024-02-22 14:03:50 +0100

    done

---
+++ whatever84545/whatever84545/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatever84545
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(uhmmm(new string[] { "slovo", "vetsi nez 5", "neco" }, 5));
+			Console.ReadKey(true);
+		}
+
+		static int uhmmm(string[] strArray, int minLength) => (from x in strArray where x.Length > minLength select x).Count();
+	}
+}
